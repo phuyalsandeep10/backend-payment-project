fedf323f57035e8dbe026557a0bfe801e28a800e
SaPok5 <samippokhrel5@gmail.com>
2025-06-29 10:55:35 +0545

    deleted the error prone apps

--- backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserListCreateView, UserDetailView, UserRegistrationView,UserSessionsView
-
-urlpatterns = [
-    path('users/', UserListCreateView.as_view(), name='user-list-create'),
-    path('users/<uuid:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('sessions/', UserSessionsView.as_view(), name='user-sessions'),
-]
