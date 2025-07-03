dc216f6cb8e21bc890fb63ec8dbd18d70590c848
SaPok5 <samippokhrel5@gmail.com>
2025-07-03 16:53:59 +0545

    Add WhiteNoise middleware and configure static file settings for production

--- backend/core_config/settings.py
+++ backend/core_config/settings.py
@@ -147,6 +147,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     # Custom ngrok compatibility middleware (must be early in the chain)
@@ -250,6 +251,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# For production, use WhiteNoise to serve static files
+# http://whitenoise.evans.io/en/stable/django.html
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
