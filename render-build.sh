79b704a878bea3a99220016c27f9d4fdf1168a81
SaPok5 <samippokhrel5@gmail.com>
2025-07-08 01:21:50 +0545

    added new script

--- render-build.sh
+++ render-build.sh
@@ -9,5 +9,5 @@ pip install -r backend/requirements.txt
 cd backend
 python manage.py migrate --fake Sales_dashboard 0002_add_user_to_dailystreakrecord
 python manage.py migrate --fake Verifier_dashboard 0002_add_organization_and_user_to_auditlogs
-
+python manage.py migrate --fake authentication 0002_add_role_to_user
 python manage.py migrate
